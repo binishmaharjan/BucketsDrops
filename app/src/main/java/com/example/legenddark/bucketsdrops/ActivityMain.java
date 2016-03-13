23ed15a700a890b33dbb8e51c8f020199d8501c3
legenddark <legendbinish124@gmail.com>
2016-03-13 14:44:25 +0900

    Git Test

---
+++ app/src/main/java/com/example/legenddark/bucketsdrops/ActivityMain.java
@@ -0,0 +1,49 @@
+package com.example.legenddark.bucketsdrops;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class ActivityMain extends AppCompatActivity {
+
+    Toolbar mToolBar;
+    Button mBtnAdd;
+    private View.OnClickListener mBtnAddListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            showDialog();
+        }
+    };
+
+    private void showDialog() {
+        DialogAdd dialog = new DialogAdd();
+        dialog.show(getSupportFragmentManager(),"dialogAdd");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        mBtnAdd = (Button) findViewById(R.id.iv_button);
+        mBtnAdd.setOnClickListener(mBtnAddListener);
+        setSupportActionBar(mToolBar);
+
+
+        initBackgroundImage();
+    }
+
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(background);
+    }
+}
---
+++ app/src/main/java/com/example/legenddark/bucketsdrops/Beans/Drops.java
@@ -0,0 +1,48 @@
+package com.example.legenddark.bucketsdrops.Beans;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by legenddark on 2016/03/08.
+ */
+public class Drops extends RealmObject {
+
+    private String what;
+    @PrimaryKey
+    private long added;
+    private long when;
+    private boolean completed;
+
+    public String getWhat() {
+        return what;
+    }
+
+    public void setWhat(String what) {
+        this.what = what;
+    }
+
+    public long getAdded() {
+        return added;
+    }
+
+    public void setAdded(long added) {
+        this.added = added;
+    }
+
+    public long getWhen() {
+        return when;
+    }
+
+    public void setWhen(long when) {
+        this.when = when;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
